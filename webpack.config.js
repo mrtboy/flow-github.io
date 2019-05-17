614d8f82892a3fe10881eee20d29f01172d75f98
Mohammadreza Taleghani Esfahani <reza@wobe-systems.com>
2019-05-17 14:26:56 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports ={ 
+  mode: "development",
+  entry: "./app/Application.js",
+  output: {
+      filename: "bundle.js"
+  },
+  watch: true,
+  devtool: "source-map",
+  module: {
+      rules: [
+          {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              use: {
+                  loader: "babel-loader",
+                  options: {
+                      presets: [
+                          "env"
+                      ]
+                  }
+              }
+          }
+      ]
+  }
+}
